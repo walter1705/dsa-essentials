cfc7e5a541a9ec2388efbbb6443d5ff0cb05a38d
walter1705 <waltergranada17@gmail.com>
2025-08-23 13:06:16 -0500

    525-B: Pacha and string. State: not passed.

---
+++ PC-UQ/525-B/Solution.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int m = scanner.nextInt();
+        scanner.nextLine();
+        ArrayList<Integer> arr = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+        scanner.close();
+        System.out.println(method(str, m, arr));
+    }
+
+    public static String method(String str, int m, ArrayList<Integer> values) {
+        int lengthStr = str.length();
+        StringBuilder builder = new StringBuilder(str);
+        for (int i = 0; i < m; i++) {
+            int start = values.get(i);
+            int end = lengthStr - start + 1;
+            String reversedStr = reverse(builder.substring(start - 1, end), "");
+            builder.replace(start - 1, end, reversedStr);
+        }
+        return builder.toString();
+    }
+
+    private static String reverse(String substring, String nwstring) {
+        if (substring.isEmpty())
+            return nwstring;
+        nwstring = nwstring + substring.charAt(substring.length() - 1);
+        return reverse(substring.substring(0, substring.length() - 1), nwstring);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Este repositorio contiene problemas sobree estructuras de datos y algoritmos, junto con su solucion.
 
+`El codigo de archivos java se deben copiar y pegar en una clase dentro de un proyecto java.`
